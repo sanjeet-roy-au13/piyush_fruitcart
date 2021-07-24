e91d3bc6213e0d085755df8f4a1b7eae4761b910
sanjeet <sanchit.micky@gmail.com>
2021-07-24 23:11:49 +0530

    uploading BE

---
+++ routes/categoryRouter.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const categoryController = require('../controllers/categoryController');
+const auth = require('../middleware/auth');
+const authAdmin = require('../middleware/authAdmin');
+
+router.route('/category')
+    .get(categoryController.getCategories)
+    .post(auth, authAdmin, categoryController.createCategories)
+
+router.route('/category/:id')
+    .delete(auth, authAdmin, categoryController.deleteCategory)
+    .put(auth, authAdmin, categoryController.updateCategory)
+module.exports = router
